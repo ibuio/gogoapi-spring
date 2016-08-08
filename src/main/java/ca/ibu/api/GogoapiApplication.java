0c7ee2b414296302f0d84d5b030be2f70a5b2b8a
jon <j.klang@chronometriq.ca>
2016-08-07 22:17:17 -0400

    commit application structure generated by SpringBoot

---
+++ src/main/java/ca/ibu/api/GogoapiApplication.java
@@ -0,0 +1,12 @@
+package ca.ibu.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GogoapiApplication.class, args);
+	}
+}
